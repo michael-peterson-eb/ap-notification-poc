72fd40b871cc82ad2693d19f5ec0ba132faec1ef
Michael Peterson <michael.peters@everbridge.com>
2025-12-18 16:33:08 -0500

    poc working

--- .api/apis/everbridge/types.ts
+++
@@ -1,6 +0,0 @@
-import type { FromSchema } from 'json-schema-to-ts';
-import * as schemas from './schemas';
-
-export type GenerateTokenFormDataParam = FromSchema<typeof schemas.GenerateToken.formData>;
-export type GenerateTokenResponse200 = FromSchema<typeof schemas.GenerateToken.response['200']>;
-export type GenerateTokenResponse401 = FromSchema<typeof schemas.GenerateToken.response['401']>;
